fd62fcba791579b56e73e4cda113cc802b4338cd
lenx0 <thiagofreitas1106@gmail.com>
2020-10-23 17:45:58 -0300

    Datas - COMPLETO

--- Test/src/application/Main.java
+++ Test/src/application/Main.java
@@ -1,28 +1,43 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
-	
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner scan = new Scanner(System.in);
-		
-		List <String> list = new ArrayList<>();
-		System.out.println("Quantos pokémon deseja cadastrar?");
-		int n = scan.nextInt();
-		for(int i = 0; i<n; i++) {
-			System.out.println("Digite o nome do pokémon #"+i);
-			String poke = scan.next();
-			list.add(poke);
+		System.out.println("Digite o tamanho da linha");
+		int L = scan.nextInt();
+		System.out.println("Digite o tamanho da coluna:");
+		int C = scan.nextInt();
+		int[][] mat = new int[L][C];
+		System.out.println("Digite agora os valores da matriz");
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				mat[i][j] = scan.nextInt();
+			}
 		}
-		System.out.println(list.contains("charmeleon"));
-		System.out.println(list.indexOf("charizard"));
-		
-		
+		System.out.println("Digite o número que deseja encontrar na matriz");
+		int N = scan.nextInt();
+		for (int i = 0; i < mat.length; i++)
+			for (int j = 0; j < mat[i].length; j++) {
+				if (mat[i][j] == N) {
+					System.out.println("Posição: " + i + "," + j);
+					if (j > 0) {
+						System.out.println("LEFT: " + mat[i][j - 1]);
+					}
+					if (j > 0) {
+						System.out.println("RIGHT: " + mat[i][j + 1]);
+					}
+					if (i > 0) {
+						System.out.println("UP: " + mat[i - 1][j]);
+					}
+					if (i < mat.length - 1) {
+						System.out.println("DOWN: " + mat[i + 1][j]);
+					}
+				}
+			}
 	}
 }
